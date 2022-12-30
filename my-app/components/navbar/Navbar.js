35bf026d16c5b6c4af2e913888f072676563fd39
Piotr Rajmanski <prajmanski10@gmail.com>
2022-12-30 12:01:54 +0100

    Navbar html added

--- my-app/components/navbar/Navbar.js
+++ my-app/components/navbar/Navbar.js
@@ -1,5 +1,36 @@
 import styles from './Navbar.module.css';
 
-export const Navbar = () => {
-    return <div>NAVBAR!</div>
+export const Navbar = (props) => {
+
+    const {username} = props;
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <a className={styles.logoLink} href="/">
+                    <div className={styles.logoWrapper}>
+                        Netflix
+                    </div>
+                </a>
+            </div>
+            <ul className={styles.navItems}>
+                <li className={styles.navItem}>Home</li>
+                <li className={styles.navItem2}>My List</li>
+            </ul>
+
+            <nav className={styles.navContaier}>
+                <div>
+                    <button className={styles.usernameBtn}>
+                        <p className={styles.username}>{username}</p>
+                    </button>
+
+                    <div className={styles.navDropdown}>
+                        <div>
+                        <a className={styles.linkName}>Sign out</a>
+                        <div className={styles.lineWrapper}></div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 }
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,10 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import { Banner } from '../components/banner/Banner'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import { Banner } from "../components/banner/Banner";
+import { Navbar } from "../components/navbar/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -15,12 +16,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Netflix</h1>
-
-      {/* <Navbar /> */}
-      <Banner title="Forrest Gump" subTitle='A history of Forrest Gump' imgUrl='https://1.bp.blogspot.com/-yb-5UYWGn-s/YOgZx-2KtJI/AAAAAAAEJag/xwdbzJDViikSLkeaA9W3vM_ZhXGuQxy1gCLcBGAsYHQ/s2048/tom-hanks-in-forrest-gump-1.jpeg'/>
+      <Navbar username="piotrek@test.pl"/>
+      <Banner
+        title="Forrest Gump"
+        subTitle="A history of Forrest Gump"
+        imgUrl="https://1.bp.blogspot.com/-yb-5UYWGn-s/YOgZx-2KtJI/AAAAAAAEJag/xwdbzJDViikSLkeaA9W3vM_ZhXGuQxy1gCLcBGAsYHQ/s2048/tom-hanks-in-forrest-gump-1.jpeg"
+      />
 
       {/* <Card /> */}
     </>
-  )
+  );
 }
