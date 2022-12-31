f47e54f4c0f3e019143cb6c4d61420737dcb0d78
Piotr Rajmanski <prajmanski10@gmail.com>
2022-12-31 14:40:49 +0100

    Added navbar once agian

---
+++ my-app/components/navbar/Navbar.js
@@ -0,0 +1,65 @@
+import styles from "./Navbar.module.css";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useState } from "react";
+import Image from "next/image";
+
+export const Navbar = (props) => {
+  const { username } = props;
+  const router = useRouter();
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleOnClickHome = (e) => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  const handleOnClickMyList = (e) => {
+    e.preventDefault();
+    router.push("/browse/my-list");
+  };
+
+  const handleShowDropdown = () => {
+    showDropdown ? setShowDropdown(false) : setShowDropdown(true);
+  }
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}><Image src={'/static/netflix.svg'} alt="Netflix logo"
+                width="111"
+                height="30" /></div>
+        </a>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
+        </ul>
+
+        <nav className={styles.navContaier}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username} onClick={handleShowDropdown}>{username}</p>
+              <Image src={'/static/arrow.svg'} alt="Dropdown Icon"
+                width="24"
+                height="24" />
+            </button>
+            {showDropdown && (
+              <div className={styles.navDropdown}>
+                <div>
+                  <Link href="/login" className={styles.linkName}>
+                    Sign out
+                  </Link>
+                  <div className={styles.lineWrapper}></div>
+                </div>
+              </div>
+            )}
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
