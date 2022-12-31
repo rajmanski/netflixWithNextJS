da611ec7451e4196dc24560c4381394a184fef1f
Piotr Rajmanski <prajmanski10@gmail.com>
2022-12-31 14:12:19 +0100

    Dropdown on navigation is fully working

--- my-app/components/navbar/Navbar.js
+++ my-app/components/navbar/Navbar.js
@@ -1,51 +1,59 @@
-import styles from './Navbar.module.css';
-import { useRouter } from 'next/router';
-import  Link  from 'next/link';
+import styles from "./Navbar.module.css";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useState } from "react";
 
 export const Navbar = (props) => {
+  const { username } = props;
+  const router = useRouter();
+  const [showDropdown, setShowDropdown] = useState(false);
 
-    const {username} = props;
-    const router = useRouter();
+  const handleOnClickHome = (e) => {
+    e.preventDefault();
+    router.push("/");
+  };
 
-    const handleOnClickHome = (e) => {
-        e.preventDefault();
-        router.push('/');
-    }
+  const handleOnClickMyList = (e) => {
+    e.preventDefault();
+    router.push("/browse/my-list");
+  };
 
-    const handleOnClickMyList = (e) => {
-        e.preventDefault();
-        router.push('/browse/my-list');
-    }
-    return (
-        <div className={styles.container}>
-            <div className={styles.wrapper}>
-                <a className={styles.logoLink} href="/">
-                    <div className={styles.logoWrapper}>
-                        Netflix
-                    </div>
-                </a>
-            <ul className={styles.navItems}>
-                <li className={styles.navItem} onClick={handleOnClickHome}>Home</li>
-                <li className={styles.navItem2} onClick={handleOnClickMyList}>My List</li>
-            </ul>
+  const handleShowDropdown = () => {
+    showDropdown ? setShowDropdown(false) : setShowDropdown(true);
+  }
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}>Netflix</div>
+        </a>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
+        </ul>
 
-            <nav className={styles.navContaier}>
+        <nav className={styles.navContaier}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username} onClick={handleShowDropdown}>{username}</p>
+            </button>
+            {showDropdown && (
+              <div className={styles.navDropdown}>
                 <div>
-                    <button className={styles.usernameBtn}>
-                        <p className={styles.username}>{username}</p>
-                    </button>
-
-                    <div className={styles.navDropdown}>
-                        <div>
-                        <Link href="/login" className={styles.linkName}>
-                            Sign out
-                        </Link>
-                        <div className={styles.lineWrapper}></div>
-                        </div>
-                    </div>
+                  <Link href="/login" className={styles.linkName}>
+                    Sign out
+                  </Link>
+                  <div className={styles.lineWrapper}></div>
                 </div>
-            </nav>
-            </div>
-        </div>
-    )
-}
+              </div>
+            )}
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
