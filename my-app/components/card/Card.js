dc72bc17c2cbee46f7920f7a9f819a90f51ef2d8
Piotr Rajmanski <prajmanski10@gmail.com>
2023-01-02 10:49:47 +0100

    Image error are handled

--- my-app/components/card/Card.js
+++ my-app/components/card/Card.js
@@ -1,21 +1,27 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import styles from './Card.module.css';
 
 export const Card = (props) => {
 
-    const {imgUrl, size} = props;
+    const {imgUrl = '/static/tom-hanks-in-forrest-gump-1.jpeg', size = 'medium'} = props;
     
+    const [imgSrc, setImgSrc] = useState(imgUrl);
     const classMap = {
-        'large': styles.lgItem,
-        'medium': styles.mgItem,
-        'small': styles.smItem, 
+        large: styles.lgItem,
+        medium: styles.mdItem,
+        small: styles.smItem, 
+    }
+
+    const handleOnError = () => {
+        setImgSrc('/static/tom-hanks-in-forrest-gump-1.jpeg');
     }
 
     return (
         <div className={styles.container}>
             Card
             <div className={classMap[size]}>
-                <Image src={imgUrl} alt="image" layout='fill' className={styles.cardImg}/>
+                <Image src={imgSrc} alt="image" layout="fill" className={styles.cardImg} onError={handleOnError}/>
             </div>
         </div>
     )
