f83e3d86fc17c85287b8267fbbd7d020c9c0e8fa
Piotr Rajmanski <prajmanski10@gmail.com>
2023-01-02 15:03:02 +0100

    Error handling on fetching data

--- my-app/lib/videos.js
+++ my-app/lib/videos.js
@@ -1,19 +1,22 @@
-import videoData from '../data/videos.json';
-
 export const getVideos = async (searchQuery) => {
-    const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
-    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchQuery}&key=${YOUTUBE_API_KEY}`
-    console.log(url)
+  const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+  const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchQuery}&key=${YOUTUBE_API_KEY}`;
+  console.log(url);
 
-    const response =  await fetch(url);
+  try {
+    const response = await fetch(url);
     const data = await response.json();
     console.log(data);
 
     return data.items.map((item) => {
-        return {
-          title: item.snippet.title,
-          imgUrl: item.snippet.thumbnails.high.url,
-          id: item?.id?.videoId || item?.id?.channelId,
-        };
+      return {
+        title: item.snippet.title,
+        imgUrl: item.snippet.thumbnails.high.url,
+        id: item?.id?.videoId || item?.id?.channelId,
+      };
     });
-}
+  } catch (error) {
+    console.log("Something went wrong with fetching the data: ", error);
+    return [];
+  }
+};
