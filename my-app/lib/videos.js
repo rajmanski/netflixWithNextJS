1ac434705e45432cc57338e2f25e3e7c5f4fbb34
Piotr Rajmanski <prajmanski10@gmail.com>
2023-01-03 20:36:30 +0100

    Padding on the bottom of the page added

--- my-app/lib/videos.js
+++ my-app/lib/videos.js
@@ -1,12 +1,10 @@
 export const getVideos = async (searchQuery) => {
   const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
   const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchQuery}&key=${YOUTUBE_API_KEY}`;
-  console.log(url);
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
 
     return data.items.map((item) => {
       return {
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -8,20 +8,22 @@ import { getVideos } from "../lib/videos";
 
 const inter = Inter({ subsets: ["latin"] });
 
-
 export async function getServerSideProps(context) {
-  const HollywoodVideos =  await getVideos('hollywood%20movies');
-  const disneyVideos =  await getVideos('disney%20trailers');
-  const travelVideos =  await getVideos('travel%20videos');
-  const kidsVideos =  await getVideos('kids%20movies%20trailers');
+  const HollywoodVideos = await getVideos("hollywood%20movies");
+  const disneyVideos = await getVideos("disney%20trailers");
+  const travelVideos = await getVideos("travel%20videos");
+  const kidsVideos = await getVideos("kids%20movies%20trailers");
   return {
-    props: { HollywoodVideos, disneyVideos, travelVideos,kidsVideos }
-  }
+    props: { HollywoodVideos, disneyVideos, travelVideos, kidsVideos },
+  };
 }
 
-export default function Home({HollywoodVideos, disneyVideos, travelVideos, kidsVideos}) {
-
-
+export default function Home({
+  HollywoodVideos,
+  disneyVideos,
+  travelVideos,
+  kidsVideos,
+}) {
   return (
     <>
       <Head>
@@ -30,20 +32,24 @@ export default function Home({HollywoodVideos, disneyVideos, travelVideos, kidsV
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar username="piotrek@test.pl"/>
-      <Banner
-        title="Forrest Gump"
-        subTitle="A history of Forrest Gump"
-        imgUrl="https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2020/09/Forrest-Gump-Tom-Hanks.jpg"
-      />
-      <div className={styles.sectionWrapper}>
-        <SectionCard title={'Hollywood'} videos={HollywoodVideos} size='large'/>
-        <SectionCard title={'Disney'} videos={disneyVideos} size='small'/>
-        <SectionCard title={'Travel'} videos={travelVideos} size='medium'/>
-        <SectionCard title={'For Kids'} videos={kidsVideos} size='small'/>
+      <div className={styles.main}>
+        <Navbar username="piotrek@test.pl" />
+        <Banner
+          title="Forrest Gump"
+          subTitle="A history of Forrest Gump"
+          imgUrl="https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2020/09/Forrest-Gump-Tom-Hanks.jpg"
+        />
+        <div className={styles.sectionWrapper}>
+          <SectionCard
+            title={"Hollywood"}
+            videos={HollywoodVideos}
+            size="large"
+          />
+          <SectionCard title={"Disney"} videos={disneyVideos} size="small" />
+          <SectionCard title={"Travel"} videos={travelVideos} size="medium" />
+          <SectionCard title={"For Kids"} videos={kidsVideos} size="small" />
+        </div>
       </div>
-      
-
     </>
   );
 }
