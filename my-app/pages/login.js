a536f61dbd15eb503e6eecb1b5ce423894e0daa3
Piotr Rajmanski <prajmanski10@gmail.com>
2023-01-04 09:59:36 +0100

    Email validation on /login added

--- my-app/pages/login.js
+++ my-app/pages/login.js
@@ -1,12 +1,37 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useState } from "react";
 import styles from "../styles/Login.module.css";
 
 const Login = () => {
+
+const [msgForUser, setMsgForUser] = useState(null);
+const [email, setEmail] = useState(null);
+
+const router = useRouter();
+
   const handleOnClick = (e) => {
-    console.log("Clicked.");
     e.preventDefault();
+    if (!email) {
+        setMsgForUser("You didn't enter your email");
+    } else {
+        console.log('This email is wrong: ', email);
+        setMsgForUser('Email that you have entered is wrong');
+        if (email === 'prajmanski10@gmail.com') {
+            setMsgForUser(null);
+            console.log('You are logged as: ', email);
+            console.log('Redirect to dashboard');
+            router.push('/');
+        }
+    }
   };
+
+  const getEmail = (e) => {
+    setEmail(e.target.value);
+  }
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -35,9 +60,9 @@ const Login = () => {
               Ready to watch? Enter your email to create or restart your
               membership.
             </h4>
-            <p className={styles.userMsg}></p>
+            <p className={styles.userMsg}>{msgForUser}</p>
             <div className={styles.inputSection}>
-              <input className={styles.emailInput} type="text" placeholder="Email" />
+              <input className={styles.emailInput} type="text" placeholder="Email" onChange={getEmail}/>
               <button className={styles.loginBtn} onClick={handleOnClick}>Get started</button>
             </div>
           </div>
