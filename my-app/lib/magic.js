ac65cbeb4489b52c8b97fea2a6396d09be84b2e2
Piotr Rajmanski <prajmanski10@gmail.com>
2023-01-04 10:45:58 +0100

    Local var changed for a next_public key

--- my-app/lib/magic.js
+++ my-app/lib/magic.js
@@ -1,6 +1,6 @@
 import { Magic } from 'magic-sdk';
 
-const MAGIC_KEY = process.env.MAGIC_KEY
+const MAGIC_KEY = process.env.NEXT_PUBLIC_MAGIC_KEY
 export const createMagic = () => {
     return (typeof window !== 'undefined' && 
     new Magic(MAGIC_KEY));
