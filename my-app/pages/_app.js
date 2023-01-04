4693fed9dfa9e412e98183f01d856627c3f33377
Piotr Rajmanski <prajmanski10@gmail.com>
2023-01-04 11:57:54 +0100

    Redirect added - if user is not logged in, it can't go to the homepage

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,5 +1,22 @@
 import '../styles/globals.css'
+import {magic} from '../lib/magic';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkIsLogged = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn();
+      if (isLoggedIn) {
+        router.push('/');
+      } else {
+        router.push('/login');
+      }
+    }
+    checkIsLogged();
+  }, [])
   return <Component {...pageProps} />
 }
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -26,7 +26,7 @@ export default function Home({
   kidsVideos,
 }) {
 
-
+  
 
   return (
     <>
